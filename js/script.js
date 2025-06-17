cac7699e0ae7a5d3d326801d98680bcdcc8a9ffe
Zura Hosting <rasyaml1131@gmail.com>
2025-06-17 18:41:37 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Hamburger toggle menu (mobile)
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
